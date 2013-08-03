8350e9e9faa4aece1ac45f339fa64bc514a3f172
sidben <sidbenetti@gmail.com>
2013-08-03 02:52:50 -0300

    Added custom icon according to the block clicked

--- code/sidben/cartocraft/client/PlayerEventHandler.java
+++ code/sidben/cartocraft/client/PlayerEventHandler.java
@@ -60,21 +60,87 @@ public class PlayerEventHandler {
                 // Get what the player used and what he or she clicked.
                 ItemStack usedItem = event.entityPlayer.getCurrentEquippedItem();
                 int targetBlockID = event.entityPlayer.worldObj.getBlockId(event.x, event.y, event.z);
+                int targetMeta = event.entityPlayer.worldObj.getBlockMetadata(event.x, event.y, event.z);
 
                 // Check if the player right-clicked with a map in hand.
                 // Process only valid blocks
                 if (usedItem != null && usedItem.itemID == Item.map.itemID) {
+                    
+                    // Defines the custom icon based on the block clicked
+                    byte targetIcon = -1;
+                    
+                    // --- Wool
+                    if (targetBlockID == Block.cloth.blockID) {
+                        switch (targetMeta) {
+                        case 0: targetIcon = 14; break;        // White
+                        case 1: targetIcon = 9; break;         // Orange
+                        case 2: targetIcon = 8; break;         // Magenta
+                        case 3: targetIcon = 6; break;         // Light Blue
+                        case 4: targetIcon = 15; break;        // Yellow
+                        case 5: targetIcon = 7; break;         // Lime
+                        case 6: targetIcon = 10; break;        // Pink
+                        case 7: targetIcon = 4; break;         // Gray
+                        case 8: targetIcon = 13; break;        // Light Gray
+                        case 9: targetIcon = 3; break;         // Cyan
+                        case 10: targetIcon = 11; break;       // Purple
+                        case 11: targetIcon = 1; break;        // Blue
+                        case 12: targetIcon = 2; break;        // Brown
+                        case 13: targetIcon = 5; break;        // Green
+                        case 14: targetIcon = 12; break;       // Red
+                        case 15: targetIcon = 0; break;        // Black
+                        }
+                    }
 
-                    // --- TEMP: Brick ---/
-                    if (targetBlockID == Block.brick.blockID) {
-                        System.out.println("---------------------------------------------------");
-                        System.out.println("Valid");
-                        System.out.println("X: " + event.x + " (byte: " + (byte)event.x + "), Z: " +event.z+ " (byte: " +(byte)event.z+ ")");
-                        System.out.println("---------------------------------------------------");
-                        
+                    // --- Mob Spawner
+                    else if (targetBlockID == Block.mobSpawner.blockID) {
+                        targetIcon = 40;
+                    }
+
+                    // --- Beacon
+                    else if (targetBlockID == Block.beacon.blockID) {
+                        targetIcon = 41;
+                    }
+                    
+                    // --- Chest, Trapped Chest, Ender Chest
+                    else if (targetBlockID == Block.chest.blockID || targetBlockID == Block.chestTrapped.blockID || targetBlockID == Block.enderChest.blockID) {
+                        targetIcon = 42;
+                    }
+
+                    // --- Nether Portal
+                    else if (targetBlockID == Block.portal.blockID) {
+                        targetIcon = 43;
+                    }
+                    
+                    // --- End Portal
+                    else if (targetBlockID == Block.endPortal.blockID || targetBlockID == Block.endPortalFrame.blockID) {
+                        targetIcon = 44;
+                    }
+                    
+                    // --- Brewing Stand
+                    else if (targetBlockID == Block.brewingStand.blockID) {
+                        targetIcon = 45;
+                    }
+                    
+                    // --- Enchantment Table, Bookshelf
+                    else if (targetBlockID == Block.enchantmentTable.blockID || targetBlockID == Block.bookShelf.blockID) {
+                        targetIcon = 46;
+                    }
+                    
+                    // --- Note Block, Jukebox
+                    else if (targetBlockID == Block.music.blockID || targetBlockID == Block.jukebox.blockID) {
+                        targetIcon = 47;
+                    }
+                    
+                    // --- Anvil
+                    else if (targetBlockID == Block.anvil.blockID) {
+                        targetIcon = 52;
+                    }
+
+                    
+                    // Adds the custom icon
+                    if (targetIcon > -1) {
                         MapData mapdata = ((ItemMap)usedItem.getItem()).getMapData(usedItem, event.entityPlayer.worldObj);
-                        mapdata.addCustomIcon((byte)3, event.x, event.z);
-                        
+                        mapdata.addCustomIcon(targetIcon, event.x, event.z);
                     }
 
                 }
